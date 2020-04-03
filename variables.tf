a7ce6e88e30ee5dd02fa767b39ce726107e65e19
Josh <josh.espinas@xero.com>
2020-04-04 00:05:24 +1300

    Adds gcp default variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,23 @@
 variable "kthw" {
   type        = string
-  description = "abbreviation for the course name"
+  description = "Abbreviation for the course name."
   default     = "kubernetes-the-hard-way"
 }
 
+variable "gcp_region" {
+  type = string
+  description = "Default region for the GCP project."
+  default = "us-west1"
+}
+
+variable "gcp_zone" {
+  type = string
+  description = "Default zone for the GCP project."
+  default = "us-west1-c"
+}
+
+variable "gcp_project" {
+  type = string
+  description = "The GCP project name."
+  default = "solid-linker-238123"
+}
