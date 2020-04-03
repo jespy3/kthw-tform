ec3e3ed1a7f95b7723b6e911b90ff9dd39923f3f
Josh <josh.espinas@xero.com>
2020-04-03 21:14:00 +1300

    Creates a subnet for kubes

--- network.tf
+++ network.tf
@@ -1,5 +1,14 @@
-resource "google_compute_network" "vpc_network" {
+// VPC
+resource "google_compute_network" "kubernetes_the_hard_way" {
   name = "kubernetes-the-hard-way"
-  description = "A dedicated custom VPC to host a kubernetes cluster"
+  description = "A dedicated custom VPC to host a kubernetes cluster."
   auto_create_subnetworks = false
+}
+
+// Subnet
+resource "google_compute_subnetwork" "kubernetes" {
+  name          = "kubernetes"
+  description   = "A group of IPs within the VPC for the kubernetes cluster."
+  ip_cidr_range = "10.240.0.0/24"
+  network       = google_compute_network.kubernetes_the_hard_way.self_link
 }
